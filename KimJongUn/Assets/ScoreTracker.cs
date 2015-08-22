453f7c6fc0a5567b94fd92e17b3eeb2c7d237276
Anthony Colford <anthony.colford@gmail.com>
2015-08-21 23:07:05 -0300

    Added the scoretracker to the main menu

---
+++ KimJongUn/Assets/ScoreTracker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreTracker : MonoBehaviour {
+	public int[] scores;
+	// Use this for initialization
+	void Start () {
+		scores = new int[5];
+		for (int i = 0; i < scores.Length;i++){
+			scores[i] = -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
