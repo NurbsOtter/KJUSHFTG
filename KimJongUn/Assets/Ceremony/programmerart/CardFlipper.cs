db422772653b82844967c41c406223288a5a77fa
Anthony Colford <anthony.colford@gmail.com>
2015-08-22 00:47:29 -0300

    Added Basic card.

---
+++ KimJongUn/Assets/Ceremony/programmerart/CardFlipper.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+public interface ICardFlip: UnityEngine.EventSystems.IEventSystemHandler{
+	void FlipCard(float r, float g, float b);
+}
+
+public class CardFlipper : MonoBehaviour, ICardFlip {
+	SpriteRenderer mySprite;
+	public float flipTime = 1.0f;
+	public float timeSinceFlip = 0.0f;
+	public bool flipping = true;
+	public bool colorSwapped = false;
+	public Color newColor;
+	// Use this for initialization
+	void Start () {
+		mySprite = GetComponent<SpriteRenderer> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		if (flipping) {
+			if (timeSinceFlip <= flipTime) {
+				Vector3 newRot = this.transform.rotation.eulerAngles;
+				newRot.x = (timeSinceFlip/flipTime) * 180.0f;
+				this.transform.rotation = Quaternion.Euler(newRot);
+				//Debug.Log(timeSinceFlip / flipTime);
+				if ((timeSinceFlip/flipTime) > .5f && !colorSwapped){
+					mySprite.color = newColor;
+					colorSwapped = true;
+				}
+				timeSinceFlip += Time.deltaTime;
+
+			}else{
+				timeSinceFlip = 0.0f;
+				flipping = false;
+				colorSwapped = false;
+				this.transform.rotation = Quaternion.Euler(Vector3.zero);//Shhh. Nobody will know.
+			}
+		}
+	}
+	public void FlipCard(float r, float g, float b){
+		newColor = new Color (r, g, b);
+		flipping = true;
+	}
+}
---
+++ KimJongUn/Assets/Ceremony/programmerart/FlipControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+/// <summary>
+/// GLORIOUS FLIPPER OF THE PEOPLE
+/// </summary>
+public class FlipControl : MonoBehaviour {
+	GameObject[,] flippers;
+	public GameObject sign;
+	public int x, y;
+	public Texture2D loadImage(string inImage){
+		Texture2D newImg = null;
+		byte[] inBytes;
+		if (File.Exists (inImage)) {
+			inBytes = File.ReadAllBytes (inImage);
+			newImg = new Texture2D (640, 480);
+			newImg.LoadImage (inBytes);
+		} else {
+			Debug.Log("Not found!");
+		}
+		return newImg;
+	}
+
+	// Use this for initialization
+	void Start () {
+		flippers = new GameObject[x,y];
+		for (int i = 0; i < x; i++) {
+			for (int i2 = 0; i2 < y; i2++){
+				flippers[i,i2] = (GameObject)Instantiate(sign,new Vector3(i,i2,0.0f),Quaternion.identity);
+			}
+		}
+		//loadImage (Application.dataPath+ "/Ceremony/programmerart/gloriousleader.png");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
