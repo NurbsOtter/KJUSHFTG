7d07099da7564e8d32d21a98f48d4b06b4a13faa
Anthony Colford <anthony.colford@gmail.com>
2015-08-22 02:33:32 -0300

    Started on Camera controls
    Very very basic!

---
+++ KimJongUn/Assets/Ceremony/programmerart/CamController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamController : MonoBehaviour {
+	public float camSpeed = 1.0f;
+	public float camSpeedZoomed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newMove = new Vector3 ();
+		newMove.x += Input.GetAxis ("Mouse X") * camSpeed * Time.deltaTime;
+		newMove.y += Input.GetAxis ("Mouse Y") * camSpeed * Time.deltaTime;
+		newMove += this.transform.position;
+		this.transform.position = newMove;
+	}
+}
