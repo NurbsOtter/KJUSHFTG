aa99b3d32685d7f2a629f6264d3cebd7742c6b36
Anthony Colford <anthony.colford@gmail.com>
2015-08-23 21:58:52 -0300

    Added GUI Cues for which seed is selected

--- KimJongUn/Assets/Farm/SeedTosser.cs
+++ KimJongUn/Assets/Farm/SeedTosser.cs
@@ -6,19 +6,26 @@ public class SeedTosser : MonoBehaviour {
 	private int curSeed = 0;
 	public Quaternion cakeRot;
 	public Collider player;
+	public GameObject canvas;
 	// Use this for initialization
 	void Start () {
 		cakeRot = Quaternion.Euler (new Vector3 (-90.0f, 0.0f, 0.0f));
+		canvas = GameObject.Find ("Canvas");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
 			curSeed = 0;
+			canvas.SendMessage("setWeapon",0,SendMessageOptions.RequireReceiver);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			curSeed = 1;
+			canvas.SendMessage("setWeapon",1,SendMessageOptions.RequireReceiver);
 		}
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
 			Vector3 newPos = transform.position + transform.forward * 1.5f;
-			GameObject newSeed = (GameObject)Instantiate(seeds[0],newPos,cakeRot);
+			GameObject newSeed = (GameObject)Instantiate(seeds[curSeed],newPos,cakeRot);
 			Rigidbody seedPhys = newSeed.GetComponent<Rigidbody>();
 			seedPhys.AddForce(transform.forward * 50.0f);
 		}
